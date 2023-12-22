26d467ee1b4557e55e6770f9f635ea3b8b554532
Andyauk <47583709+Andyauk@users.noreply.github.com>
2023-12-22 01:14:23 +0000

    post

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version "adamant"
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game "rdr3"
+
+author 'rexshack'
+description 'rsg-interiors'
+version '1.0.0'
+
+client_scripts {
+    'interiors.lua'
+}
+
+server_scripts {
+    'versionchecker.lua'
+}
+
+dependency {
+    'rsg-core'
+}
